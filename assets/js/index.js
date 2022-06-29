de29b03b80d837aa5b7c3714667b105dd6d3bbcc
Ivy Ronoh <ronoh.ivy@gmail.com>
2022-06-29 14:25:02 +0300

    Added Event Listeners for Event RSVP

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,15 +2,16 @@ document.addEventListener('DOMContentLoaded', () => {
   // const projectUrl = "https://i-ronoh.github.io/dog-events-api/db.json";
   const projectUrl = "http://localhost:3000/events"
 
-// CONTAINERS
-let eventHandler = document.getElementById("eventHandler")
+  
+  // CONTAINERS
+  let eventHandler = document.getElementById("eventHandler")
 
   //Get Events
   function getEvents () {
     let reqOptions = {
       method: 'GET'
     }
-
+    
     fetch (projectUrl, reqOptions)
     .then ((response) => response.json())
     .then (results => {
@@ -18,13 +19,13 @@ let eventHandler = document.getElementById("eventHandler")
       results.forEach(eventItem => {
         displayEvents(eventItem)
         // console.log (eventItem)
-  })
-  // .catch((err)=>
-  //   console.log(err))
-  })
-  
+      })
+      // .catch((err)=>
+      //   console.log(err))
+    })
+    
   }
-
+  
   // Display Event Card
   function displayEvents(eventCard) {
     let eventsData =  
@@ -35,23 +36,73 @@ let eventHandler = document.getElementById("eventHandler")
       <h4>Location: ${eventCard.location}</p>
       <h4>Date: ${eventCard.date}</p>
       <h4>Cover Charge: ${eventCard.entry}</p>
-      <span ><a href="" id="like">&hearts; likes</a></span>
+      <br>
+      <div class="like">
+                <i id="thumbsUp" class="fa fa-thumbs-o-up" aria-hidden="true"> Attending</i>
+                <i id="thumbsDown" class="fa fa-thumbs-o-down" aria-hidden="true"> Not Interested</i>
+              </div>
+      </div>
+      `
+      
+      eventHandler.innerHTML += eventsData
+      
+      let attend = document.querySelector('.fa-thumbs-o-up');
+      attend.addEventListener('click', (event) => {
+        attend.style.color = 'orange'
+        notInterested.style.color = 'white'
+        alert ('Looking forward to seeing you there!')
 
-    </div>
-    `
-    eventHandler.innerHTML += eventsData
+      })
 
-  }
-
-  const newsletter = document.getElementById('footer-email-btn');
-  newsletter.addEventListener('click', (event) => {
-    alert ('Thank you for subscribing to our newsletter!')
-    footer-email.innerHTML.remove()
+      let notInterested = document.querySelector('.fa-thumbs-o-down');
+      notInterested.addEventListener('click', (event) => {
+        notInterested.style.color = 'orange'
+        alert ('Shucks! Sure hope to see you next time!')
+        attend.style.color = 'white'
+      })
+      
+      //adding like functionality
+      // const like = 10;
+      
+      // function like(){
+        //     likeCount++;
+        // }
+        
+        // document.querySelector("#like")
+        // like.addEventListener("click", (e) => {
+          //   const liker = document.getElementById("like")
+          
+          //   like.style.color = "blue"
+          //   liker.innerText = '${eventCard.likes}' + 1
+          
+          // })
+          
+          // const like = document.querySelector("#like")
+          // like.addEventListener('click', (e) => {
+            //   e.preventDefault()
+            //   console.log("liker")
+            //   like.getElementsByClassName.color = 'red'
+            // })
+          }
+          
+          // let attend = document.getElementById('thumbsUp');
+          // attend.addEventListener('click', (event) => {
+          //   alert ('Looking forward to seeing you there!')
+          // })
+          
+          
+          const newsletter = document.getElementById('footer-email-btn');
+          newsletter.addEventListener('click', (event) => {
+            alert ('Thank you for subscribing to our newsletter!')
+    `${footer-email}`.innerHTML.reset()
   })
-
-
-
+  
   getEvents()
+
 })
-    
+
+// //GET QUOTE
+// const quote = document.getElementById('quotation');
+// quote.addEventListener('click', (event) => {
+// alert (Math.floor((Math.random() * 2500) + 1000))
 
