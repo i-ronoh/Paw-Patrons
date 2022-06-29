42c7ded0ccf34a28334f5987afa2b615295a5088
Ivy Ronoh <ronoh.ivy@gmail.com>
2022-06-29 06:52:53 +0300

    appended json data into html card elements

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,51 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const projectUrl = "https://i-ronoh.github.io/dog-events-api/db.json";
+  // const projectUrl = "https://i-ronoh.github.io/dog-events-api/db.json";
+  const projectUrl = "http://localhost:3000/events"
+
+// CONTAINERS
+let eventHandler = document.getElementById("eventHandler")
+
+  //Get Events
+  function getEvents () {
+    let reqOptions = {
+      method: 'GET'
+    }
+
+    fetch (projectUrl, reqOptions)
+    .then ((response) => response.json())
+    .then (results => {
+      let eventsData = ""
+      results.forEach(eventItem => {
+        displayEvents(eventItem)
+        // console.log (eventItem)
+  })
+  // .catch((err)=>
+  //   console.log(err))
+  })
   
+  }
+
+  // Display Event Card
+  function displayEvents(eventCard) {
+    let eventsData =  
+    `
+    <div class="event-data">
+    <img src="${eventCard.image}" width="300" height="500" alt="eventImg">
+      <h3>${eventCard.name}</p>
+      <h4>Location: ${eventCard.location}</p>
+      <h4>Date: ${eventCard.date}</p>
+      <h4>Cover Charge: ${eventCard.entry}</p>
+    </div>
+    `
+    eventHandler.innerHTML += eventsData
+
+  }
 
 
 
 
 
-
-
-
-
-
-});
+  getEvents()
+})
     
 
